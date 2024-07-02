6a464659432c0d7b6cdab791e0d370416d4c7868
Paulinha159 <paularebesco@gmail.com>
2024-07-02 11:01:55 -0300

    editei

--- Areas.js
+++ Areas.js
@@ -1,12 +1,13 @@
-function Areacirculo (raio){
-    return 3.14*(raio**2)
+function Areacirculo(raio) {
+    return 3.14 * (raio ** 2)
 }
-console.log (Areacirculo(2))
-function areatriangulo(base, altura){
-    return (base*altura)/2
+function areatriangulo(base, altura) {
+    return (base * altura) / 2
 }
-console.log(areatriangulo(3,5))
-function arearetangulo(base, altura){
-    return base*altura
+function arearetangulo(base, altura) {
+    return base * altura
 }
-console.log (arearetangulo( 5,6))
+
+module.exports = {
+    Areacirculo, arearetangulo, areatriangulo
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const area = require ("./Areas.js")
+console.log(area.Areacirculo(2))
+console.log(area.areatriangulo(6,3))
+console.log(area.arearetangulo(9,5))
