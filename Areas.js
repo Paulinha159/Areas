b7978ffcd1a0214941d9631f9cf4bbf13e48e716
Paulinha159 <paularebesco@gmail.com>
2024-07-02 10:52:13 -0300

    edit

---
+++ Areas.js
@@ -0,0 +1,12 @@
+function Areacirculo (raio){
+    return 3.14*(raio**2)
+}
+console.log (Areacirculo(2))
+function areatriangulo(base, altura){
+    return (base*altura)/2
+}
+console.log(areatriangulo(3,5))
+function arearetangulo(base, altura){
+    return base*altura
+}
+console.log (arearetangulo( 5,6))
